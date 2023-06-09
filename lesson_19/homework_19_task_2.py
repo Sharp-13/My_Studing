4a1b762339a56c55d3670937c2918572351f3b0b
Sharp-13 <sergey.sharp.1987@gmail.com>
2023-06-09 22:45:55 +0200

    Homework lesson 19

--- lesson_19/homework_19_task_2.py
+++ lesson_19/homework_19_task_2.py
@@ -4,9 +4,17 @@
 
 def in_range(start, end, step = 1):
     n = start
-    while n <= end:
-        yield n
-        n += step
+    if step == 0:
+        raise ValueError('Step can not be zero')
+    elif step > 0:
+        while n <= end:
+            yield n
+            n += step
+    else:
+        while n >= end:
+            yield n
+            n += step
 
-for i in in_range(3, 8, 2):
+for i in in_range(8, 3, -2):
     print(i)
+
