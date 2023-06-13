18082815d3810154923962ea49656166e5889a22
Sharp-13 <sergey.sharp.1987@gmail.com>
2023-06-13 22:15:55 +0200

    Homework lesson 32

--- lesson_32/task1_client.py
+++ lesson_32/task1_client.py
@@ -4,8 +4,8 @@ HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-    sock.connect((HOST, PORT))
-    sock.sendall(b'Hello, world')
+    # sock.bind((HOST, PORT))
+    sock.sendto(b'Hello, world', (HOST, PORT))
     data = sock.recv(1024)
 
 print('Received', repr(data))
---
+++ lesson_32/task1_server.py
@@ -0,0 +1,27 @@
+import socket
+
+udpsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+host = '127.0.0.1'
+port = 65432
+print('starting up on {} port {}'.format(host, port))
+udpsock.bind((host, port))
+
+while True:
+    # Wait for a connection
+    print('waiting for a connection')
+    # udpsock.recvfrom(1024)
+    data, client_address = udpsock.recvfrom(1024)
+    try:
+        print('connection from', client_address)
+        print('received {!r}'.format(data))
+        if data:
+            print('sending data back to the client')
+            udpsock.sendto(data.upper(), client_address)
+        else:
+            print('no data from', client_address)
+            break
+
+    finally:
+        # Clean up the connection
+        udpsock.close()
